7459d85cff0c13e78b54e3355ede31fd5daf1911
lceberg <kaloyan.github@gmail.com>
2016-12-28 15:58:40 +0200

    Add guess input focus on load

--- react/src/components/GuessForm.js
+++ react/src/components/GuessForm.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 class GuessForm extends React.Component {
+	componentDidMount(){
+		this.guessInput.focus();
+	}
+
 	render() {
 		return <form id="guess-form">
-			<input id="guess-input" type="text" maxLength="4" value="" />
+			<input id="guess-input" type="text" maxLength="4" value="" ref={(input) => { this.guessInput = input; }}  />
 			<input id="guess-submit" type="submit" value="Check" />
 		</form>;
 	}
